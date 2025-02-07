c0b7a5dae10b6e8202a7dd6cc2c87b38b46b1609
ChinmayJoshi98 <joshichinmay94@gmail.com>
2025-02-07 15:36:06 -0600

    My imiage added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import myImage from "../assets/img/myImage.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -9,18 +9,17 @@ export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [delta, setDelta] = useState(200 - Math.random() * 50);
   const [index, setIndex] = useState(1);
   const toRotate = [ "Full Stack Developer", "Certified MuleSoft Developer" ];
-  const period = 500;
+  const period = 300;
 
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, delta);
-
     return () => { clearInterval(ticker) };
-  }, [text])
+  }, [text, delta]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
@@ -41,7 +40,7 @@ export const Banner = () => {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
-      setDelta(500);
+      setDelta(200);
     } else {
       setIndex(prevIndex => prevIndex + 1);
     }
@@ -50,15 +49,24 @@ export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="aligh-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Whadup! I'm Chinmay, a`} <span className="txt-rotate" dataPeriod="30" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                <span className="tagline">Innovative. Passionate. Driven.</span>
+                <h1>
+                  Hi, I'm Chinmay Joshi, a{" "}
+                  <span className="txt-rotate" dataPeriod="300" data-rotate='[ "Full Stack Developer", "Certified MuleSoft Developer" ]'>
+                    <span className="wrap">{text}</span>
+                  </span>
+                </h1>
+                <p>
+                  I specialize in building scalable web applications and robust APIs that empower businesses to excel in a digital world.
+                </p>
+                <button onClick={() => console.log('Let’s Connect clicked')}>
+                  Let’s Connect <ArrowRightCircle size={25} />
+                </button>
               </div>}
             </TrackVisibility>
           </Col>
@@ -66,7 +74,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
+                  <img src={myImage} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
           </Col>
